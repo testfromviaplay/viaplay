927afe4c330c9c3d9bfb9e9a8319521be532949b
U-BIGBEN-PC\bigben <Jia Li>
2017-02-12 19:17:48 +0100

    add existing files

---
+++ viaplay/tests/fake_movie_info.py
@@ -0,0 +1,8 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/pc-se/film/ted-2-2015')
+def imdb_info():
+    return '_embedded["viaplay:blocks"][0]._embedded["viaplay:product"].content.imdb:"tt0137523"', 200
---
+++ viaplay/tests/start_fake.sh
@@ -0,0 +1,2 @@
+export FLASK_APP=./fake_movie_info.py
+flask run --port=9090
